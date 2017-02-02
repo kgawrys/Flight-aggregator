67422524025fde5789e7ef6b20ffc7ecf06faec2
Krzysztof Gawryś <kgawrys3@gmail.com>
2017-02-02 11:19:21 +0100

    Added Macwire dependency injection

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,19 +3,25 @@ import sbt._
 object Version {
   val akkaV        = "2.4.10"
   val logbackV     = "1.1.9"
+  val macwireV     = "2.2.5"
 }
 
 object Library {
   import Version._
 
-  val akkaActor        = "com.typesafe.akka"        %% "akka-actor"                        % akkaV
-  val akkaSfl4j        = "com.typesafe.akka"        %% "akka-slf4j"                        % akkaV
-  val akkaStream       = "com.typesafe.akka"        %% "akka-stream"                       % akkaV
-  val akkaHttpCore     = "com.typesafe.akka"        %% "akka-http-core"                    % akkaV
-  val akkaHttp         = "com.typesafe.akka"        %% "akka-http-experimental"            % akkaV
-  val akkaSprayJson    = "com.typesafe.akka"        %% "akka-http-spray-json-experimental" % akkaV
+  val akkaActor        = "com.typesafe.akka"         %% "akka-actor"                        % akkaV
+  val akkaSfl4j        = "com.typesafe.akka"         %% "akka-slf4j"                        % akkaV
+  val akkaStream       = "com.typesafe.akka"         %% "akka-stream"                       % akkaV
+  val akkaHttpCore     = "com.typesafe.akka"         %% "akka-http-core"                    % akkaV
+  val akkaHttp         = "com.typesafe.akka"         %% "akka-http-experimental"            % akkaV
+  val akkaSprayJson    = "com.typesafe.akka"         %% "akka-http-spray-json-experimental" % akkaV
 
-  val logback          = "ch.qos.logback"            % "logback-classic"                   % logbackV
+  val logback          = "ch.qos.logback"            % "logback-classic"                    % logbackV
+
+  // macwire
+  val macwireMacros    = "com.softwaremill.macwire"  %% "macros"                            % macwireV
+  val macwireUtil      = "com.softwaremill.macwire"  %% "util"                              % macwireV
+
 }
 
 object Dependencies {
@@ -23,6 +29,8 @@ object Dependencies {
 
   val apiCore = Seq(
     akkaActor, akkaSfl4j, akkaStream, akkaHttpCore, akkaHttp, akkaSprayJson,
-    logback
+    logback,
+    macwireMacros % "provided",
+    macwireUtil
   )
 }
