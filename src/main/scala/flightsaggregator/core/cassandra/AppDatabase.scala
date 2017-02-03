8cc784719e6251beca48875b355fa2ae6c7dac95
Krzysztof Gawryś <kgawrys3@gmail.com>
2017-02-03 15:06:06 +0100

    Cassandra Repository

--- src/main/scala/flightsaggregator/core/cassandra/AppDatabase.scala
+++ src/main/scala/flightsaggregator/core/cassandra/AppDatabase.scala
@@ -2,6 +2,8 @@ package flightsaggregator.core.cassandra
 
 import com.websudos.phantom.connectors.KeySpaceDef
 import com.websudos.phantom.dsl._
+import flightsaggregator.repository.FlightStatesWithConnector
 
 class AppDatabase(val keyspace: KeySpaceDef) extends Database(keyspace) {
+  object flightstates extends FlightStatesWithConnector with keyspace.Connector
 }
